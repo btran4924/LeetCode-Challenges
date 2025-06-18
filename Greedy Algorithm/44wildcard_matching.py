01657f34c5e35004ad9c5310ed02fa1507df7c2b
btran4924 <benjamintran4924@gmail.com>
2025-06-18 02:24:17 -0400

    Uploaded Challege #2: Wildcard Matching (also changing how entries are named)

---
+++ Greedy Algorithm/44wildcard_matching.py
@@ -0,0 +1,34 @@
+## Traverse both lists:
+##    Traverse until *, once hit, keep moving until match again, if not, then fail.
+##    Every time ?, move onto next value.
+## Return true when both list have been traversed completely (at the same time)
+## Way to attack:
+##     (Deprecated) Most likely will compare both lists to eachother, anytime no match (unless ?), False
+##     New attempt: DP: iterate until "*", s compare with p until match, then iterate again. If ?, just move on no comparison.
+
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        p1 = p2 = 0
+        match = 0
+        star = -1
+        n, m = len(s), len(p)
+
+        while p1 < n:
+            if p2 < m and (p[p2] == s[p1] or p[p2] == '?'):
+                p1 += 1
+                p2 += 1
+            ## Start of checking if its a match
+            elif p2 < m and p[p2] == '*':
+                star = p2
+                match = p1
+                p2 += 1
+            elif star != -1:
+                p2 = star + 1
+                match += 1
+                p1 = match
+            else: 
+                return False
+        
+        while p2 < m and p[p2] == '*':
+            p2 += 1
+        return p2 == m
